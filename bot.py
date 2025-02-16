c969eb63241c30b5d1af8f1d08fc102ece89edf7
Mridul Seth <git@mriduls.com>
2025-02-16 17:41:36 +0100

    stochastic garlic

--- bot.py
+++ bot.py
@@ -1,35 +1,50 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-import numpy as np
 from tilthenightends import Levelup, LevelupOptions, Vector, Team, Towards
+import random
 
-
-RNG = np.random.default_rng(seed=12)
-
-
-class Leader:
-    def __init__(self, hero: str):
-        self.hero = hero
-        self.next_turn = 5.0
-        self.vector = Vector(1, 1)
-
-    def run(self, t, dt, monsters, players, pickups) -> Vector | Towards | None:
-        if t > self.next_turn:
-            self.vector = Vector(*RNG.random(2) * 2 - 1)
-            self.next_turn += 5.0
-        return self.vector
-
-
-class Follower:
-    def __init__(self, hero: str, following: str):
-        self.hero = hero
-        self.following = following
-
-    def run(self, t, dt, monsters, players, pickups) -> Vector | Towards | None:
-        for name, player in players.items():
-            if name == self.following:
-                return Towards(player.x, player.y)
-        return None
+useful_levelup_options = {
+    "evelyn": [  # Magic Wand
+        LevelupOptions.player_health,
+        LevelupOptions.player_speed,
+        LevelupOptions.weapon_health,
+        LevelupOptions.weapon_cooldown,
+        LevelupOptions.weapon_damage,
+        LevelupOptions.weapon_size,
+        LevelupOptions.weapon_speed,
+        LevelupOptions.weapon_longevity,
+    ],
+    "theron": [  # Garlic
+        LevelupOptions.player_health,
+        LevelupOptions.player_speed,
+        LevelupOptions.weapon_damage,
+        LevelupOptions.weapon_size,
+    ],
+    "alaric": [  # Fireball
+        LevelupOptions.player_health,
+        LevelupOptions.player_speed,
+        LevelupOptions.weapon_health,
+        LevelupOptions.weapon_cooldown,
+        LevelupOptions.weapon_damage,
+        LevelupOptions.weapon_size,
+    ],
+    "isolde": [  # Holy Water
+        LevelupOptions.player_health,
+        LevelupOptions.player_speed,
+        LevelupOptions.weapon_cooldown,
+        LevelupOptions.weapon_damage,
+        LevelupOptions.weapon_size,
+    ],
+    "selene": [  # Mine
+        LevelupOptions.player_health,
+        LevelupOptions.player_speed,
+        LevelupOptions.weapon_cooldown,
+        LevelupOptions.weapon_damage,
+        LevelupOptions.weapon_size,
+        LevelupOptions.weapon_health,
+        LevelupOptions.weapon_longevity,
+    ],
+}
 
 
 class Brain:
@@ -37,19 +52,38 @@ class Brain:
         pass
 
     def levelup(self, t: float, info: dict, players: dict) -> Levelup:
-        # A very random choice
-        hero = RNG.choice(list(players.keys()))
-        what = RNG.choice(list(LevelupOptions))
-        return Levelup(hero, what)
+        alive_players = {p:player for p, player in players.items() if player.health > 0}
+        current_player = random.choice(list(alive_players.keys()))
+        option = random.choice(useful_levelup_options[current_player])
+        return Levelup(current_player, option)
 
+class GoToDifferent5Directions:
+    def __init__(self, hero: str, direction: int):
+        self.hero = hero
+        self.direction = direction
+
+    def run(self, t, dt, monsters, players, pickups) -> Vector | Towards | None:
+        match self.direction:
+            case 0:
+                return Towards(100_000, 100_000)
+            case 1:
+                return Towards(-100_000, 100_000)
+            case 2:
+                return Towards(100_000, -100_000)
+            case 3:
+                return Towards(-100_000, -100_000)
+            case 4:
+                return Towards(0, 0)
+            case _:
+                raise ValueError
 
 team = Team(
     players=[
-        Leader(hero="alaric"),
-        Follower(hero="kaelen", following="alaric"),
-        Follower(hero="garron", following="alaric"),
-        Follower(hero="isolde", following="alaric"),
-        Follower(hero="lyra", following="alaric"),
+        GoToDifferent5Directions(hero="isolde", direction=0),
+        GoToDifferent5Directions(hero="theron", direction=1),
+        GoToDifferent5Directions(hero="alaric", direction=2),
+        GoToDifferent5Directions(hero="selene", direction=3),
+        GoToDifferent5Directions(hero="evelyn", direction=4),
     ],
     strategist=Brain(),
 )
